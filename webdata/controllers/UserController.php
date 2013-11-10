da9ec219d1b05a8afebc338aa07527ee2d1f7468
Ronny Wang <ronnywang@gmail.com>
2013-11-10 17:10:31 +0800

    add jqgrid

--- webdata/controllers/UserController.php
+++ webdata/controllers/UserController.php
@@ -7,6 +7,44 @@ class UserController extends Pix_Controller
         $this->view->user = $params['user'];
     }
 
+    public function getdataAction()
+    {
+        list(, /*data*/, /*getdata*/, $id) = explode('/', $this->getURI());
+        if (!$set = DataSet::find(intval($id))) {
+            return $this->redirect('/');
+        }
+        $data_lines = DataLine::search(array('set_id' => $set->set_id));
+
+        $page = intval($_GET['page']);
+        $limit = intval($_GET['rows']);
+        $sidx = intval($_GET['sidx']);
+        $sord = 'asc' == $_GET['sord'] ? 'asc' : 'desc';
+
+        $ret = new StdClass;
+        $ret->page = $page;
+        $ret->total = ceil(count($data_lines) / $limit);
+        $ret->records = count($data_lines);
+        $ret->rows = array();
+
+        $num_cols = json_decode($set->getEAV('num_cols'));
+        if (1 == $num_cols[$sidx]) {
+            $order_string = "(data->>{$sidx})::numeric {$sord}";
+        } else {
+            $order_string = "(data->>{$sidx}) {$sord}";
+        }
+
+        $sql = "SELECT * FROM \"data_line\" WHERE (\"set_id\" = {$set->set_id})";
+        $sql .= " ORDER BY {$order_string}";
+        $sql .= " LIMIT {$limit}";
+        $sql .= " OFFSET " . ($limit * $page - $limit);
+        $res = DataLine::getDb()->query($sql);
+        while ($db_row = $res->fetch_assoc()) {
+            $row = array('id' => $db_row['id'], 'cell' => json_decode($db_row['data']));
+            $ret->rows[] = $row;
+        }
+        return $this->json($ret);
+    }
+
     public function importcsvAction()
     {
         $user = $_GET['user'];
