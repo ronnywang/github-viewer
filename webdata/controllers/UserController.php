667ceebc6488a722485885f01b3190a81edfd723
Ronny Wang <ronnywang@gmail.com>
2013-11-10 12:27:50 +0800

    加上 /user/index /user/repo /user/blob

---
+++ webdata/controllers/UserController.php
@@ -0,0 +1,24 @@
+<?php
+
+class UserController extends Pix_Controller
+{
+    public function indexAction($params)
+    {
+        $this->view->user = $params['user'];
+    }
+
+    public function blobAction($params)
+    {
+        $this->view->user = $params['user'];
+        $this->view->repository = $params['repository'];
+        $this->view->branch = $params['branch'];
+        $this->view->path = $params['path'];
+    }
+
+    public function repoAction($params)
+    {
+        $this->view->user = $params['user'];
+        $this->view->repository = $params['repository'];
+        $this->view->path = $params['path'];
+    }
+}
