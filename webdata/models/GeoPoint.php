99e226608acc648a3364ee51b351214b65fee23d
Ronny Wang <ronnywang@gmail.com>
2013-11-11 17:09:52 +0800

    add GeoPoint

---
+++ webdata/models/GeoPoint.php
@@ -0,0 +1,22 @@
+<?php
+
+class GeoPoint extends Pix_Table
+{
+    public function init()
+    {
+        $this->_name = 'geo_point';
+
+        $this->_primary = array('point_id');
+
+        $this->_columns['point_id'] = array('type' => 'int', 'auto_increment' => true);
+        $this->_columns['group_id'] = array('type' => 'int');
+        $this->_columns['geo'] = array('type' => 'geography', 'modifier' => array('point', 4326));
+
+        // GeoPoint::getDb()->query("CREATE INDEX geo_point_geo ON geo_point USING GIST(geo)");
+    }
+
+    public function _getDb()
+    {
+        return DataLine::getDb();
+    }
+}
