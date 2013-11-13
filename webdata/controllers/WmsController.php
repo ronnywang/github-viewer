1f8df70216b3ddb95e4326dc540ba8b9e27a2373
Ronny Wang <ronnywang@gmail.com>
2013-11-13 17:35:41 +0800

    加上 GeoJSON 支援

--- webdata/controllers/WmsController.php
+++ webdata/controllers/WmsController.php
@@ -47,11 +47,80 @@ class WmsController extends Pix_Controller
         if ($layer_data->type == 'csv') {
             return $this->drawCSV(intval($layer_data->set_id), $options);
         } elseif ($layer_data->type == 'geojson') {
+            return $this->drawGeoJSON(intval($layer_data->set_id), $options);
         }
     }
 
     protected function drawGeoJSON($set_id, $options)
     {
+        if (!$dataset = DataSet::find($set_id)) {
+            echo '404';
+            return $this->noview();
+        }
+        $lng_delta = $options['max_lng'] - $options['min_lng'];
+
+        if ($lng_delta < 0.01) {
+            $tolerance = "0.000001";
+        } else if ($lng_delta < 0.1) {
+            $tolerance = "0.00001";
+        } else if ($lng_delta < 1) {
+            $tolerance = "0.0001";
+        } else if ($lng_delta < 10) {
+            $tolerance = "0.001";
+        } else {
+            $tolerance = "0.01";
+        }
+
+        $sql = "SELECT id, ST_AsGeoJSON(ST_Simplify(geo::geometry, {$tolerance})) AS geojson FROM data_geometry WHERE set_id= {$set_id} AND geo && ST_GeomFromText('{$options['text']}')";
+        //$sql = "SELECT MIN(data_id) AS data_id, ST_AsGeoJSON(ST_Centroid(ST_Collect(geo::geometry))) AS geojson FROM (SELECT kmeans(ARRAY[ST_X(geo::geometry), ST_Y(geo::geometry)], 1000) OVER (), geo, data_id FROM geo_point WHERE group_id = {$group_id} AND geo && ST_GeomFromText('$text')) AS ksub GROUP BY kmeans";
+        //error_log($sql);
+        $res = DataGeometry::getDb()->query($sql);
+
+        $json = new StdClass;
+        $json->type = 'FeatureCollection';
+
+        $features = array();
+        //echo $sql . "\n";
+        $geojsons = array();
+        while ($row = $res->fetch_assoc()) {
+            $geojsons[$row['id']] = $row['geojson'];
+        }
+        $res->free_result();
+
+        if (!count($geojsons)) {
+            return $this->noview();
+        }
+
+        foreach ($geojsons as $id => $geojson) {
+            $feature = new StdClass;
+            $feature->type = 'Feature';
+            $feature->properties = array('background_color' => array(0,0,0), 'border_color' => array(100, 100, 100));
+            if (!json_decode($geojson)->geometries[0]) {
+                continue;
+            }
+            $feature->geometry = json_decode($geojson)->geometries[0];
+            $features[] = $feature;
+        }
+        $json->features = $features;
+
+        $obj = new GeoJSON2Image($json);
+        $obj->setSize($options['width']);
+        $obj->setBoundry(array($options['min_lng'], $options['max_lng'], $options['min_lat'], $options['max_lat']));
+        $obj->draw();
+
+        $time[3] = microtime(true);
+
+        /*error_log(sprintf("total: %f, pgsql: %f(%d), mysql: %f(%d), png: %f, bbox: %s",
+            $time[3] - $time[0],
+            $time[1] - $time[0],
+            count($geojsons),
+            $time[2] -$time[1],
+            count($records),
+            $time[3] - $time[2],
+            $bbox));
+         */
+
+        return $this->noview();
     }
 
     protected function drawCSV($set_id, $options)
@@ -95,20 +164,9 @@ class WmsController extends Pix_Controller
             return $this->noview();
         }
 
-        $records = array();
-        foreach (DataLine::search(array('set_id' => $dataset->set_id))->searchIn('id', array_keys($geojsons)) as $data_line) {
-            $records[$data_line->id] = json_decode($data_line->data);
-        }
         $time[2] = microtime(true);
 
-        $max_rate = 1;
-        $min_rate = 0;
-
         foreach ($geojsons as $id => $geojson) {
-            if (!$record = $records[$id]) {
-                continue;
-            }
-
             $feature = new StdClass;
             $feature->type = 'Feature';
             $feature->properties = array('background_color' => array(255,0,0));
