6e5ded90b43e6c17f4e80454091bbc1d8768dbd0
Ronny Wang <ronnywang@gmail.com>
2013-11-14 13:54:24 +0800

    add get_content function

--- webdata/scripts/geojson_parse.js
+++ webdata/scripts/geojson_parse.js
@@ -6,6 +6,8 @@ fs.readFile(process.argv[3], function(err, data){
     var json = JSON.parse(data);
     if ('get_type' == process.argv[2]) {
         console.log(json.type);
+    } else if ('get_content' == process.argv[2]) {
+        console.log((new Buffer(json.content, 'base64')).toString());
     } else if ('split_feature' == process.argv[2]) {
         for (var i = 0; i < json.features.length; i ++) {
             fs.writeFileSync(process.argv[4] + '/' + i + '.json', JSON.stringify(json.features[i]));
