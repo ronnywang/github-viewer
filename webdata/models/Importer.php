8fbff889e01fc434061dfb61c113c1ed7dbdf31a
Ronny Wang <ronnywang@gmail.com>
2013-11-15 16:20:50 +0800

    改用 Helper::getTmpFile()

--- webdata/models/Importer.php
+++ webdata/models/Importer.php
@@ -11,6 +11,7 @@ class Importer
         $url = 'https://api.github.com/repos/' . urlencode($user) . '/' . urlencode($repository) . '/contents/' . urlencode($path);
         $curl = curl_init($url);
         curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($curl, CURLOPT_ENCODING, 'gzip,deflate'); 
         curl_setopt($curl, CURLOPT_HTTPHEADER, array('Authorization: token ' . getenv('GITHUB_TOKEN')));
         $ret = curl_exec($curl);
         curl_close($curl);
@@ -33,21 +34,22 @@ class Importer
         $download_fp = tmpfile();
         curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
         curl_setopt($curl, CURLOPT_FILE, $download_fp);
+        curl_setopt($curl, CURLOPT_ENCODING, 'gzip,deflate'); 
         curl_setopt($curl, CURLOPT_HTTPHEADER, array('Authorization: token ' . getenv('GITHUB_TOKEN')));
         curl_exec($curl);
         curl_close($curl);
         fflush($download_fp);
 
         // 這邊解 base64 真的只能求助外部啊 orz
-        $result_fp = tmpfile();
+        $result_file = Helper::getTmpFile();
         $script_file = __DIR__ . '/../scripts/geojson_parse.js';
-        $cmd = "node " . escapeshellarg($script_file) . " get_content " . escapeshellarg(stream_get_meta_data($download_fp)['uri']) . " > " . escapeshellarg(stream_get_meta_data($result_fp)['uri']);
+        $cmd = "node " . escapeshellarg($script_file) . " get_content " . escapeshellarg(stream_get_meta_data($download_fp)['uri']) . " > " . escapeshellarg($result_file);
         exec($cmd, $outputs, $ret);
-
-        fclose($download_fp);
         if ($ret) {
-            throw new Importer_Exception("Fetch content failed");
+            throw new Importer_Exception("Get content from git_url failed");
         }
-        return stream_get_meta_data($result_fp)['uri'];
+        fclose($download_fp);
+
+        return $result_file;
     }
 }
