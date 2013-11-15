93c2e0df0708a07bfd84576a8c08d43b562be6b4
Ronny Wang <ronnywang@gmail.com>
2013-11-15 16:19:54 +0800

    add Helper::getTmpFile()

---
+++ webdata/models/Helper.php
@@ -0,0 +1,40 @@
+<?php
+
+class Helper
+{
+    protected static $_tmp_file_used = null;
+
+    public static function getTmpFile()
+    {
+        if (is_null(self::$_tmp_file_used)) {
+            self::$_tmp_file_used = array();
+            register_shutdown_function(array('Helper', 'deleteTmpFile'));
+        }
+        $file_name = tempnam('', 'HelperGetTmpFile-');
+        self::$_tmp_file_used[] = $file_name;
+        return $file_name;
+    }
+
+    public static function deleteTmpFile()
+    {
+        foreach (self::$_tmp_file_used as $file) {
+            self::deleteFile($file);
+        }
+    }
+
+    protected static function deleteFile($file)
+    {
+        if (is_dir($file)) {
+            $d = opendir($file);
+            while ($f = readdir($d)) {
+                if ($f == '.' or $f == '..') {
+                    continue;
+                }
+                self::deleteFile($file . '/' . $f);
+            }
+            rmdir($file);
+        } else if (is_file($file)) {
+            unlink($file);
+        }
+    }
+}
