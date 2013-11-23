6a4d47c9b80561e3aeebf929bc920b31e677646f
Ronny Wang <ronnywang@gmail.com>
2013-11-23 15:57:01 +0800

    加上 FileBranchMap model

---
+++ webdata/models/FileBranchMap.php
@@ -0,0 +1,20 @@
+<?php
+
+class FileBranchMap extends Pix_Table
+{
+    public function init()
+    {
+        $this->_name = 'file_branch_map';
+        $this->_primary = array('user', 'repository', 'path', 'branch');
+
+        $this->_columns['user'] = array('type' => 'varchar', 'size' => 32);
+        $this->_columns['repository'] = array('type' => 'varchar', 'size' => 32);
+        $this->_columns['path'] = array('type' => 'varchar', 'size' => 255);
+        $this->_columns['branch'] = array('type' => 'varchar', 'size' => 16);
+        $this->_columns['created_at'] = array('type' => 'int', 'default' => 0);
+        $this->_columns['updated_at'] = array('type' => 'int', 'default' => 0)
+
+        $this->_columns['commit'] = array('type' => 'char', 'size' => 40);
+    }
+}
+
