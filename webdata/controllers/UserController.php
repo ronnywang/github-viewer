d4b5f2d17c364bfacaf26e870709ea7d1a1b2d20
Ronny Wang <ronnywang@gmail.com>
2013-12-09 13:17:27 +0800

    change to ST_Union and add ST_Buffer

--- webdata/controllers/UserController.php
+++ webdata/controllers/UserController.php
@@ -13,11 +13,13 @@ class UserController extends Pix_Controller
         $lat = floatval($_GET['lat']);
         $lng = floatval($_GET['lng']);
 
+        $pixel = 0.001;
+
         if ($layer->type == 'geojson') {
             if (!$set = DataSet::find(intval($layer->set_id))) {
                 return $this->redirect('/');
             }
-            $sql = "SELECT id, ST_AsGeoJSON(merge_geo) AS json FROM (SELECT id, ST_UnaryUnion(geo::geometry) as merge_geo FROM data_geometry WHERE set_id = {$set->set_id} AND geo && ST_PointFromText('POINT({$lng} {$lat})', 4326) GROUP BY id) AS merged_polygon WHERE ST_Contains(merge_geo, ST_GeomFromText('POINT({$lng} {$lat})', 4326))";
+            $sql = "SELECT id, ST_AsGeoJSON(merge_geo) AS json FROM (SELECT id, ST_Buffer(ST_Union(geo::geometry), {$pixel} * 2) as merge_geo FROM data_geometry WHERE set_id = {$set->set_id} AND geo && ST_PointFromText('POINT({$lng} {$lat})', 4326) GROUP BY id) AS merged_polygon WHERE ST_Contains(merge_geo, ST_GeomFromText('POINT({$lng} {$lat})', 4326))";
             $res = DataGeometry::getDb()->query($sql);
             if (!$row = $res->fetch_assoc()) {
                 return $this->json(array('error' => true, 'message' => 'not found'));
@@ -38,7 +40,7 @@ class UserController extends Pix_Controller
             if (!$dataset = DataSet::find($set->getEAV('data_from'))){
                 return $this->redirect('/');
             }
-            $sql = "SELECT id, ST_AsGeoJSON(merge_geo) AS json FROM (SELECT id, ST_UnaryUnion(geo::geometry) as merge_geo FROM data_geometry WHERE set_id = {$mapset->set_id} AND geo && ST_PointFromText('POINT({$lng} {$lat})', 4326) GROUP BY id) AS merged_polygon WHERE ST_Contains(merge_geo, ST_GeomFromText('POINT({$lng} {$lat})', 4326))";
+            $sql = "SELECT id, ST_AsGeoJSON(merge_geo) AS json FROM (SELECT id, ST_Buffer(ST_Union(geo::geometry), {$pixel} * 2) as merge_geo FROM data_geometry WHERE set_id = {$mapset->set_id} AND geo && ST_PointFromText('POINT({$lng} {$lat})', 4326) GROUP BY id) AS merged_polygon WHERE ST_Contains(merge_geo, ST_GeomFromText('POINT({$lng} {$lat})', 4326))";
             $res = DataGeometry::getDb()->query($sql);
             if (!$row = $res->fetch_assoc()) {
                 return $this->json(array('error' => true, 'message' => 'not found'));
